a20cc20581e947a8b07c1adc8e1d41a33bba0e5b
lishuai <249782785@qq.com>
2018-01-18 23:38:29 +0800

    view 转html

--- vueshop/src/router/index.js
+++ vueshop/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import GoodsList from '@/views/GoodsList'
+import Cart from '@/views/Cart'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/goodsList',
+      name: 'GoodsList',
+      component: GoodsList
+    },
+    {
+      path: '/cart',
+      name: 'Cart',
+      component: Cart
     }
   ]
 })
