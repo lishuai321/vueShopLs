c1c9afec93c5c08c7d80b19d04d76923b4f19d4e
lishuai <249782785@qq.com>
2018-01-20 09:32:30 +0800

    价格区间的样式绑定

--- vueshop/src/main.js
+++ vueshop/src/main.js
@@ -3,7 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import VueLazyload from 'vue-lazyload';
+Vue.use(VueLazyload,{
+  loading:"static/loading-svg/loading-bars.svg",
+  try:3
+})
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
--- vueshop/src/views/GoodsList.vue
+++ vueshop/src/views/GoodsList.vue
@@ -8,11 +8,11 @@
             <span class="sortby">排序:</span>
             <a href="javascript:void(0)" class="default cur">默认</a>
             <a href="javascript:void(0)" class="price">价格 <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
-            <a href="javascript:void(0)" class="filterby">筛选</a>
+            <a href="javascript:void(0)" class="filterby" @click.stop="showFilterPop">筛选</a>
           </div>
           <div class="accessory-result">
             <!-- filter -->
-            <div class="filter" id="filter">
+            <div class="filter" id="filter" :class="{'filterby-show':filterBy}">
               <dl class="filter-price">
                 <dt>价格区间:</dt>
                 <dd><a href="javascript:void(0)" @click="setPriceFilter('all')" :class="{'cur':priceChecked=='all'}">选择价格</a></dd>
@@ -28,7 +28,7 @@
                 <ul>
                     <li v-for="item in goodsList">
                       <div class="pic">
-                        <a href="#"><img :src="'/static/'+item.productImage" alt=""></a>
+                        <a href="#"><img v-lazy="'/static/'+item.productImage" alt=""></a>
                       </div>
                       <div class="main">
                         <div class="name">{{item.productName}}</div>
@@ -44,6 +44,7 @@
           </div>
         </div>
       </div>
+      <div class="md-overlay" v-show="filterBy" @click="closePop"></div>
       <nav-footer></nav-footer>
     </div>
 </template>
@@ -61,6 +62,7 @@ export default {
     return{
       goodsList:[],
       priceChecked:"all",
+      filterBy:false,
       priceFilter:[
       {
         startPrice:'0.00',
@@ -103,6 +105,12 @@ export default {
     },
     setPriceFilter(index){
       this.priceChecked = index;
+    },
+    showFilterPop(){
+      this.filterBy = true;
+    },
+    closePop(){
+      this.filterBy= false;
     }
   },
   mounted(){
