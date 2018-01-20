4827b2fba5590d0446ce84eab0d2c225e68f76eb
lishuai <249782785@qq.com>
2018-01-20 11:04:29 +0800

    服务端路由的修改

--- vueshop/server/routes/users.js
+++ vueshop/server/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('用户界面');
 });
 
 module.exports = router;
